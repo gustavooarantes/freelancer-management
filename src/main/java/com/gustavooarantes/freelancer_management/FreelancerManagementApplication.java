5d26cf421af2e0ce7f4d9fcd95fcb9cdd6e7c6f4
gustavooarantes <profissional.gustavoa@gmail.com>
2025-10-04 15:44:10 -0300

    first commit

---
+++ src/main/java/com/gustavooarantes/freelancer_management/FreelancerManagementApplication.java
@@ -0,0 +1,13 @@
+package com.gustavooarantes.freelancer_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreelancerManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreelancerManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gustavooarantes/freelancer_management/FreelancerManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gustavooarantes.freelancer_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreelancerManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
