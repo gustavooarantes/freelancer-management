66d41896a84849223afc681afe05c7535fafe515
gustavooarantes <profissional.gustavoa@gmail.com>
2025-10-09 16:39:24 -0300

    corrected typo i guess

--- src/main/java/com/gustavooarantes/freelancer_management/dto/UserRequest.java
+++ src/main/java/com/gustavooarantes/freelancer_management/dto/UserRequest.java
@@ -1,5 +1,5 @@
 package com.gustavooarantes.freelancer_management.dto;
 
 public record UserRequest(
-    String fullName, String email, String password, String cpf, String cnpj) {
+  String fullName, String email, String password, String cpf, String cnpj) {
 }
